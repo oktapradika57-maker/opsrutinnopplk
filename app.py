1093f63ff56034deaee47802adfbb9cec28673f0
oktapradika57-maker <oktapradika57@gmail.com>
2026-06-22 12:28:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown("""
     }
     .menu-box {
         height: 250px;
-        wight : 300px;
+        width : 300px;
         background: #262730;
         border-radius: 20px;
         border: 200px solid #444;
